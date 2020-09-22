1c24b698a2163f29f5067397b6709bd2fbb47b6b
GustavoBMendes <mendes.gustavob@gmail.com>
2020-09-22 16:46:18 -0300

    atividade 10 concluida

--- aula10/atividade10.s
+++ aula10/atividade10.s
@@ -2,6 +2,7 @@
 	pedeDado: .asciz "\nEntre com um numero inteiro: "
 	pedeN: .asciz "\nEntre com um valor inteiro de n: "
 	mostraSoma: .asciz "\nConteudo do reg depois da soma: %d\n"
+	mostraMult: .asciz "\nConteudo do reg depois da multiplicacao: %d\n"
 
 	dado: .int 0
 	n: .int 0
@@ -27,16 +28,22 @@ _start:
 	pushl $formato
 	call scanf
 
-	movl dado, %ebx
-
-	addb %bh, %bl
-
-	pushl %ebx
-	pushl $mostraSoma
-	call printf
-
 	soma:
-		
+		movl dado, %ebx
+		movl dado, %eax
+		rorl $16, %eax
+
+		addw %ax, %bx
+		pushl %ebx
+		pushl $mostraSoma
+		call printf
+
+	multiplicacao:
+		movl n, %ecx
+		sall %cl, %ebx
+		pushl %ebx
+		pushl $mostraMult
+		call printf
 
 	pushl $0
 	call exit
